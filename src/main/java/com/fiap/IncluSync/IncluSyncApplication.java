84f1836ea33d5fabf85b0dac498a3d918222e6eb
Pedro <pedro.phoa1405@gmail.com>
2025-11-14 19:37:01 -0300

    feat: iniciando projeto

---
+++ src/main/java/com/fiap/IncluSync/IncluSyncApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.IncluSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncluSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncluSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/IncluSync/IncluSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.IncluSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncluSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
